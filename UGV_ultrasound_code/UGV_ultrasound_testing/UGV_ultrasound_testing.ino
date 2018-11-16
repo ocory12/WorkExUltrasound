9396b3ecdb1a8d4e25e01c910e05def74910b140
ocory12 <ojcory@gmail.com>
2018-11-16 21:04:15 +0000

    Addition of UGV_ultrasound_testing

---
+++ UGV_ultrasound_code/UGV_ultrasound_testing/UGV_ultrasound_testing.ino
@@ -0,0 +1,28 @@
+#include <NewPing.h>
+
+#define MAX_DIST 200
+
+#define TRIGGER_FRONT 8
+#define ECHO_FRONT 7
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  delay(50);
+  Serial.print(8);
+  Serial.print(":");
+  Serial.print("3");
+  Serial.print(":");  
+  Serial.print("4");
+  Serial.print(":");  
+  Serial.print("5");
+  Serial.print(":");  
+  Serial.print("6");
+  Serial.println();
+}
