c7f7e6fa795b28480c9f81faa071e44935c5fbe2
Ollie C <ojcory@gmail.com>
2018-11-23 13:30:17 +0000

    addition of UGV_frontend_GUI

--- UGV_ultrasound_frontend/UGV_frontend_2/UGV_frontend_2/Program.cs
+++ UGV_ultrasound_frontend/UGV_frontend_2/UGV_frontend_2/Program.cs
@@ -5,53 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.IO.Ports;
+using System.Threading;
 
 namespace UGV_frontend_2
 {
-   // class Data
-   // {
-   //     public int serialFront;
-   //     public int serialBack;
-   //     public int serialLeft;
-   //     public int serialRight;
-   //     public int serialTop;
-   //     public Data( int SerialFront, int SerialBack, int SerialLeft, int SerialRight, int SerialTop)
-   //     {
-   //       serialFront = SerialFront;
-   //       serialBack = SerialBacl;
-   //       serialLeft = SerialLeft;
-   //       serialRight = SerialRight;
-   //       serialTop = SerialTop;
-   //     }
-   // }
+
     class Program
     {
+
+
+
         static void Main()
         {
+
+            ThreadStart newThread = new ThreadStart(DataCollection);
+            Thread thread = new Thread(newThread);
+            thread.Start();
+
+
+
+            // Console.WriteLine("Front: {0}, Back: {1}, Left: {2}, Right: {3}, Top: {4}", dataToMain.sf, dataToMain.sb, dataToMain.sl, dataToMain.sr, dataToMain.st  );
+
+        }
+
+        static void DataCollection()
+        { 
             SerialPort serialArduino = new SerialPort();
             serialArduino.PortName = "COM3";
             serialArduino.BaudRate = 9600;
             serialArduino.Open();
 
-
-            while (true)
+            public int sf;
+            public int sb;
+            public int sl;
+            public int sr;
+            public int st;
+            
+            while(true)
             {
                 string[] splitData = serialArduino.ReadLine().Split(':');
 
-                int sf = Int32.Parse(splitData[0]);
-                int sb = Int32.Parse(splitData[1]);
-                int sl = Int32.Parse(splitData[2]);
-                int sr = Int32.Parse(splitData[3]);
-                int st = Int32.Parse(splitData[4]);
 
-                Console.WriteLine("Front: {0}, Back: {1}, Left: {2}, Right: {3}, Top: {4}", sf, sb, sl, sr, st);
-            }
+                int SF = Int32.Parse(splitData[0]);
+                int SB = Int32.Parse(splitData[1]);
+                int SL = Int32.Parse(splitData[2]);
+                int SR = Int32.Parse(splitData[3]);
+                int ST = Int32.Parse(splitData[4]);
 
+            
 
+                //Console.WriteLine("Front: {0}, Back: {1}, Left: {2}, Right: {3}, Top: {4}", SF,SB,SL,SR,ST);
+           }
 
-            // Data serialTest = new Data(sf, sb, sl, sr, st);
-
-            // Console.WriteLine("Front: {0}, Back: {1}, Left: {2}, Right: {3}, Top: {4}", serialTest.serialFront, serialTest.serialBack, serialTest.serialLeft, serialTest.serialRight, serialTest.serialTop);
-        }
     }
-}
+
---
+++ UGV_ultrasound_frontend/UGV_frontend_GUI/UGV_frontend_GUI/UGV_frontend_GUI/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace UGV_frontend_GUI
+{
+    public class UGV_GUI : Form
+    {
+        public UGV_GUI()
+        {
+            InitializeComponent();
+        }
+        void InitializeComponent()
+        {
+            Paint += new PaintEventHandler(UGV_Paint);
+        }
+        private void UGV_Paint(object sender, PaintEventArgs e)
+        {
+            Pen redPen = new Pen(Color.Red, 3.00F);
+            Rectangle rectFront = new Rectangle(130,20, 250,50);
+            Rectangle rectBack = new Rectangle(130,340, 250,50);
+            Rectangle rectLeft = new Rectangle(130, 80, 50,250);
+            Rectangle rectRight = new Rectangle(330,80, 50,250);
+            e.Graphics.DrawRectangle(redPen, rectFront);
+            e.Graphics.DrawRectangle(redPen, rectBack);
+            e.Graphics.DrawRectangle(redPen, rectLeft);
+            e.Graphics.DrawRectangle(redPen, rectRight);
+
+           
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            ThreadStart newThread = new ThreadStart(DataCollection);
+            Thread thread = new Thread(newThread);
+            thread.Start();
+
+            Application.Run(new UGV_GUI());
+                 
+        }
+
+        public static void DataCollection()
+        {
+            SerialPort serialArduino = new SerialPort();
+            serialArduino.PortName = "COM3";
+            serialArduino.BaudRate = 9600;
+            serialArduino.Open();
+
+            
+
+            while (true)
+            {
+
+
+                string[] splitData = serialArduino.ReadLine().Split(':');
+
+                int sf = Int32.Parse(splitData[0]);
+                int sb = Int32.Parse(splitData[1]);
+                int sl = Int32.Parse(splitData[2]);
+                int sr = Int32.Parse(splitData[3]);
+                int st = Int32.Parse(splitData[4]);
+
+                
+               
+            }
+            
+        }
+    }
+}
